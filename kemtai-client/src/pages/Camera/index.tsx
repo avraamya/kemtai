12e7c96b9af9d98b7919523b31d5c52ede309a82
Avraam Yagudaev <avraamyagudaev@gmail.com>
2021-06-02 09:59:20 +0300

    add camera functionality

--- kemtai-client/src/pages/Camera/index.tsx
+++ kemtai-client/src/pages/Camera/index.tsx
@@ -1,5 +1,46 @@
+import React, { useEffect } from "react";
+
+//Input for the exercise
+const Reps = {
+  TimeOfMove: [1, 2.5, 3, 3.1, 4.5, 5],
+  ScoreOfMove: [60, 85, 42, 60, 70, 80],
+};
+
 function Camera() {
-  return <div>camera</div>;
+
+  //turn on camera.
+  useEffect(() => {
+    startVideo()
+  }, []);
+
+  //camera function.
+  const startVideo = () => {
+    navigator.getUserMedia(
+      {
+        video: true,
+      },
+      (stream) => {
+        let outerHtmlElement : any = document.getElementsByClassName("videoFeed"); 
+        let video = outerHtmlElement[0];
+        if (video) {
+          video.srcObject = stream;
+        }
+      },
+      (err) => console.error(err)
+    );
+  };
+
+  return (
+    <div className="camera">
+      <video
+        height="800"
+        width="800"
+        muted
+        autoPlay
+        className="videoFeed"
+      ></video>
+    </div>
+  );
 }
 
 export default Camera;
